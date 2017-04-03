de6b94d34ba38caa48088998557e4fc1991fd945
fabgame <fvarni@alice.it>
2017-04-03 15:35:44 +0200

    ridimensionamento schermo

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
   t.title = "G A L A C T I C   I N V A D E R S"
-  t.window.width = 512
-  t.window.height = 700
+  t.window.width = 800
+  t.window.height = 600
   t.window.icon ="assets/icona.png"
 end
