b5f46e02f8439ce1e402367e6d9d7b5493b73edd
fabgame <fvarni@alice.it>
2017-04-05 16:35:33 +0200

    aggiunta cassa animata a dx

---
+++ cassa.lua
@@ -0,0 +1,15 @@
+return {
+  animations = {
+    "assets/cassa/giphy1.png",
+    "assets/cassa/giphy2.png",
+    "assets/cassa/giphy3.png",
+    "assets/cassa/giphy4.png",
+    "assets/cassa/giphy5.png",
+    "assets/cassa/giphy6.png",
+    "assets/cassa/giphy7.png",
+    "assets/cassa/giphy8.png",
+    "assets/cassa/giphy9.png",
+    "assets/cassa/giphy10.png",
+    "assets/cassa/giphy11.png"
+  }
+}
