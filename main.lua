93b68066ec75b012786aa69481db88268b179320
Rulezline87 <rulezline87@gmail.com>
2017-04-07 15:32:05 +0200

    aggiustamento main e playcontroller

--- main.lua
+++ main.lua
@@ -8,6 +8,11 @@ playerController = require ("playerController")
 backgroundController = require ("backgroundController")
 meteorsController = require ("meteorsController")
 enemiesController = require ("enemiesController")
+select = require ("select")
+menu = require ("menu")
+credits = require ("credits")
+menuStart = require ("menuStart")
+gameOver = require ("gameOver")
 
 function love.load(arg)
   music = love.audio.newSource("assets/Linkin Park - Points Of Authority.mp3")
@@ -16,62 +21,148 @@ function love.load(arg)
 
   font = love.graphics.newFont("assets/kenvector_future.ttf", 20)
 
+  select.load()
+  menu.load()
+  --  stato del gioco
+  gameState = "start"
+
+  -- cambio ship
+  changeShip = 0
+
+  -- menu di selezione
+  selector = 0
+
 end
 
 function love.update(dt)
-  music:play()
-  if isPaused_music then
-    music:stop()
+  -- testo blink
+  timer = timer + dt -- aggiunge al timer il tempo intercorso tra un frame e l'altro
+
+  if(timer >= blinkTime) then     -- se il timer ha superato il tempo di "blinking"...
+    if(blinkColor == color1) then -- ... effettua il cambio di colore
+      blinkColor = color2
+    else
+      blinkColor = color1
+    end
+    timer = 0 -- azzera il timer
   end
 
-  if isPaused then return
-  end
+
+
 
   backgroundController.update(dt)
   playerController.update(dt)
   meteorsController.update(dt)
   enemiesController.update(dt)
 
-  if meteorsController.GenNumMeteors()==0 and enemiesController.GenNumEnemies() == 0 then
-    if love.math.random(10) >5 then
-        meteorsController.generateMeteors()
-    else
-        enemiesController.generateEnemies()
-    end
-  end
+  -- stato del gioco
+    if gameState == "playing" then -- start game
+      backgroundController.update(dt)
+      playerController.update(dt)
+      meteorsController.update(dt)
+      enemiesController.update(dt)
+      musicMenu:stop()
+      music:play()
+
+        if meteorsController.GenNumMeteors()==0 and enemiesController.GenNumEnemies() == 0 then
+          if love.math.random(10) >5 then
+            meteorsController.generateMeteors()
+          else
+            enemiesController.generateEnemies()
+          end
+        end
+      elseif gameState == "gameOver" then
+        music:play()
+      elseif gameState == "start" then  -- musica e press start
+        musicMenu:play()
+        function love.keypressed(key, unicode)
+          if (love.keyboard.isDown("return","space")) then
+            audio4:play()
+            gameState = "menu"
+          end
+        end
+      elseif gameState == "menu" then -- menu
+        menu.update(dt)
+
+      elseif gameState == "paused" then
+        if isPaused_music then
+          music:stop()
+        end
+
+        if isPaused then return
+        end
+        --[[function love.keypressed(key, unicode)
+          if (love.keyboard.isDown("return","escape","p")) then
+            audio3:play()
+          gameState = "playing"
+          end
+        end]]--
+      elseif gameState == "quit" then
+        love.event.push('quit')
+      elseif gameState == "select" then
+        select.update()
+      end
 end
 
 function love.draw()
-  backgroundController.draw()
-  playerController.draw()
-  meteorsController.draw()
-  enemiesController.draw()
 
-end
+  if gameState == "start" then
+    menuStart.draw()
+  elseif gameState == "gameOver" then
+    audio3:stop()
+    gameOver.draw()
 
-function love.keypressed(key, scancode, isrepeat)
-  -- se è premuto 'escape' chiude il gioco
-  if(key == "escape") then
-    love.event.push('quit')
-  end
+  elseif gameState == "credits" then
+    audio1:stop()
+    credits.draw()
+    if (love.keyboard.isDown("escape")) then
+      audio3:play()
+      gameState = "menu"
+    end
+    -- menu del gioco
+  elseif gameState == "menu" then
+    menu.draw()
 
-  if(key == "space" and playerController.status == "game over") then
-    playerController.status = "play"
-  elseif(key == "space") then
-    playerController.fireBullet()
-  end
+  elseif gameState == "paused" then -- pausa
+    button_paused()
+  elseif gameState == "select" then -- pausa
+    select.draw()
+  elseif gameState == "playing" then  -- grafica e funzioni del gioco
+    backgroundController.draw()
+    playerController.draw()
+    meteorsController.draw()
+    enemiesController.draw()
 
-  if(key == "c") then
-    isDebug = not isDebug
-    playerController.isDebug = isDebug
-    meteorsController.isDebug = isDebug
-  end
+    love.graphics.print(string.format("Press 'h' key to enter HC debug mode (currently set to: %s)", isDebug), 10, 10)
+      function love.keypressed(key, scancode, isrepeat)
+        -- se è premuto 'escape' chiude il gioco
+        if(key == "p") then
+          isPaused=not isPaused
+          audio3:play()
+          gameState = "paused"
+        end
 
-  if (key=="p") then
-    isPaused=not isPaused
-  end
+        -- se premuto chiude il gioco
+        if(key == "escape") then
+          gameState = "gameOver"
+          audio3:play()
+        end
 
-  if (key=="m") then
-    isPaused_music=not isPaused_music
-  end
+        if(key == "space" and playerController.status == "game over") then
+          playerController.status = "play"
+        elseif(key == "space") then
+          playerController.fireBullet()
+        end
+
+        if (key=="m") then
+          isPaused_music=not isPaused_music
+        end
+
+        if(key == "c") then
+          isDebug = not isDebug
+          playerController.isDebug = isDebug
+          meteorsController.isDebug = isDebug
+        end
+      end
+    end
 end
--- playerController.lua
+++ playerController.lua
@@ -7,7 +7,11 @@ local meteorsController = require ("meteorsController")
 
 local playerController = {}
 
-local sprite = "assets/player.png"
+local sprite = {"assets/img/players/ShipBlue.png",
+"assets/img/players/ShipGreen.png",
+"assets/img/players/ShipOrange.png",
+"assets/img/players/ShipRed.png"
+}
 local fire = "assets/fire03.png"
 local bulletSprite = "assets/laser01.png"
 local life = "assets/player_50_2.png"
@@ -24,9 +28,9 @@ playerController.y = 0
 playerController.img = nil
 playerController.velX = 0
 playerController.velY = 0
-playerController.acc = 2
-playerController.dec = 0.4
-playerController.maxVel = 8
+playerController.acc = 6
+playerController.dec = 0.3
+playerController.maxVel = 9
 playerController.isDebug = true
 playerController.status = "play"
 playerController.lives=3
@@ -37,7 +41,7 @@ playerController.lives=3
 
 local function createBullet()
   local img = love.graphics.newImage(bulletSprite)
-  bullet = HC.rectangle(playerController.x, playerController.y - playerController.img:getHeight() - 20, img:getWidth(), img:getHeight());
+  bullet = HC.rectangle(playerController.x, playerController.y - playerController.img[1]:getHeight() - 20, img:getWidth(), img:getHeight());
   bullet.img = img
   bullet.type = "Bullet"
   bullet.speed = 25
@@ -104,28 +108,49 @@ end
 
 -- inizializza i dati della navicella
 function playerController.load()
-  playerController.img = love.graphics.newImage(sprite)
+  playerController.img = {love.graphics.newImage(sprite[1]),
+    love.graphics.newImage(sprite[2]),
+    love.graphics.newImage(sprite[3]),
+    love.graphics.newImage(sprite[4])
+  }
   playerController.fire = love.graphics.newImage(fire)
+  playerController.width = playerController.img[1]:getWidth()
+  playerController.height = playerController.img[1]:getHeight()
   playerController.x = love.graphics.getWidth() / 2
   playerController.y = love.graphics.getHeight() - 80
   playerController.life = love.graphics.newImage(life)
 
   -- crea il poligono per le collisioni
-  playerController.shapeHC = HC.polygon(0, -60, playerController.img:getWidth() / 2, 0, -playerController.img:getWidth() / 2, 0)
+  playerController.shapeHC = HC.polygon(0, -60, playerController.img[1]:getWidth() / 2, 0, -playerController.img[1]:getWidth() / 2, 0)
 
 end
 
 function playerController.update(dt)
 
+  -- cambio astronave
+  if changeShip == 0 then
+    playerController.img[1] = playerController.img[1]
+  end
+  if changeShip == 1 then
+    playerController.img[1] = playerController.img[2]
+  end
+  if changeShip == 2 then
+    playerController.img[1] = playerController.img[3]
+  end
+  if changeShip == 3 then
+    playerController.img[1] = playerController.img[4]
+  end
+
   -- se il gioco è finito, ritorna senza aggiornare nulla
-  if playerController.status == "game over" then points=0;
-    return
+  if playerController.status == "game over" then
+    gameState = "gameOver"
+    points = 0
   end
 
   -- controllo il movimento della navicella
-  if (love.keyboard.isDown("left")) then
+  if (love.keyboard.isDown("left","a")) then
     playerController.velX = playerController.velX - playerController.acc
-  elseif(love.keyboard.isDown("right")) then
+  elseif(love.keyboard.isDown("right","d")) then
     playerController.velX = playerController.velX + playerController.acc
   elseif(playerController.velX > 0) then
     playerController.velX = playerController.velX - playerController.dec
@@ -142,12 +167,12 @@ function playerController.update(dt)
   end
 
   -- controllo il movimento orizzontale (bordi, etc.)
-  if (playerController.x < playerController.img:getWidth() / 2 - playerController.velX) then
+  if (playerController.x < playerController.img[1]:getWidth() / 2 - playerController.velX) then
     playerController.velX = 0
-    playerController.x = playerController.img:getWidth() / 2
-  elseif (playerController.x > 512 - playerController.velX - playerController.img:getWidth() / 2) then
+    playerController.x = playerController.img[1]:getWidth() / 2
+  elseif (playerController.x > 512 - playerController.velX - playerController.img[1]:getWidth() / 2) then
     playerController.velX = 0
-    playerController.x = 512 - playerController.img:getWidth() / 2
+    playerController.x = 512 - playerController.img[1]:getWidth() / 2
   else
     if(playerController.velX > playerController.maxVel) then
       playerController.velX = playerController.maxVel
@@ -157,9 +182,9 @@ function playerController.update(dt)
     playerController.x = playerController.x + playerController.velX
   end
 
-  if (love.keyboard.isDown("up")) then
+  if (love.keyboard.isDown("up","w")) then
     playerController.velY = playerController.velY - playerController.acc
-  elseif(love.keyboard.isDown("down")) then
+  elseif(love.keyboard.isDown("down","s")) then
     playerController.velY = playerController.velY + playerController.acc
   elseif(playerController.velY > 0) then
     playerController.velY = playerController.velY - playerController.dec
@@ -176,12 +201,12 @@ function playerController.update(dt)
   end
 
   -- controllo il movimento verticale (bordi, etc.)
-  if (playerController.y < playerController.img:getHeight() / 2 - playerController.velY+200) then
+  if (playerController.y < playerController.img[1]:getHeight() / 2 - playerController.velY+200) then
     playerController.velY = 0
-    playerController.y = playerController.img:getHeight() / 2+200
-  elseif (playerController.y > love.graphics.getHeight() - playerController.velY - playerController.img:getHeight() / 2) then
+    playerController.y = playerController.img[1]:getHeight() / 2+200
+  elseif (playerController.y > love.graphics.getHeight() - playerController.velY - playerController.img[1]:getHeight() / 2) then
     playerController.velY = 0
-    playerController.y = love.graphics.getHeight() - playerController.img:getHeight() / 2
+    playerController.y = love.graphics.getHeight() - playerController.img[1]:getHeight() / 2
   else
     if(playerController.velY > playerController.maxVel) then
       playerController.velY = playerController.maxVel
@@ -215,15 +240,15 @@ function playerController.update(dt)
 end
 
 function playerController.draw()
-  -- se il gioco è finito, ritorna senza disegnare nulla
+  --[[ se il gioco è finito, ritorna senza disegnare nulla
   love.graphics.setFont(font)
   if playerController.status == "game over" then
     love.graphics.printf("GAME OVER\n", 0, love.graphics.getHeight() / 2-40, love.graphics.getWidth()-280, "center")
     love.graphics.printf("\nPress spacebar to play again", 0, love.graphics.getHeight() / 2-40 + 20, love.graphics.getWidth()-280, "center")
     return
-  end
+  end ]]--
 
-  love.graphics.draw(playerController.img, playerController.x, playerController.y, 0, 1, 1, playerController.img:getWidth() / 2, playerController.img:getHeight() / 2)
+  love.graphics.draw(playerController.img[1], playerController.x, playerController.y, 0, 1, 1, playerController.img[1]:getWidth() / 2, playerController.img[1]:getHeight() / 2)
   love.graphics.setColor(255, love.math.random(255), 0, love.math.random(255))
   love.graphics.draw(playerController.fire, playerController.x, playerController.y+55, 0, 1, 1, playerController.fire:getWidth() / 2, playerController.fire:getHeight() / 2)
   love.graphics.setColor(255,255,255,255)
